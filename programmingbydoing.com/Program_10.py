2c5ec662f9cf04f4804c3ee66aa2963bb8ff0957
anwari-fikri <50336496+anwari-fikri@users.noreply.github.com>
2019-05-07 22:56:09 +0800

    Add files via upload

---
+++ programmingbydoing.com/Program_10.py
@@ -0,0 +1,21 @@
+# More Variables and Printing
+
+name = "Zed A. Shaw"
+age = 35
+height = 74
+weight = 180
+eyes = "Blue"
+teeth = "White"
+hair = "Brown"
+
+print("Let's talk about", name, ".")
+print("He's", height, "inches (or", height*0.0254, "metres) tall.")
+print("He's", weight, "pounds (or", weight*0.4535924, "kilograms) heavy.")
+print("Actually, that's not too heavy.")
+print("He's got", eyes, "eyes and", hair, "hair.")
+print("His teeth are usually", teeth, "depending on the coffee.")
+
+print("If I add", age, ",", height, ", and", weight, "I get", (age + height + weight), ".")
+
+
+
---
+++ programmingbydoing.com/Program_11.py
@@ -0,0 +1,9 @@
+# Using Variables
+
+roomNumber = 113
+eValue = 2.71828
+course = "Computer Science"
+
+print("This is room", roomNumber)
+print("e is close to", eValue)
+print("I am learning a bit about", course)
