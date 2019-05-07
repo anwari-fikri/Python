029e8760d4caf62599a06017dbe934cbbd3af9be
anwari-fikri <50336496+anwari-fikri@users.noreply.github.com>
2019-05-07 21:46:00 +0800

    Delete Program_7.py

--- Program_7.py
+++
@@ -1,9 +0,0 @@
-# Your Initials
-
-print("    A      F F F F F");
-print("  A   A    F        ");
-print("A       A  F        ");
-print("A A A A A  F F F    ");
-print("A       A  F        ");
-print("A       A  F        ");
-print("A       A  F        ");
