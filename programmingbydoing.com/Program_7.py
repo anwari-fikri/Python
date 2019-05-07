499e3ca71e3e765a5421c544da37050b9b6573ec
anwari-fikri <50336496+anwari-fikri@users.noreply.github.com>
2019-05-07 21:51:04 +0800

    Add files via upload

---
+++ programmingbydoing.com/Program_7.py
@@ -0,0 +1,9 @@
+# Your Initials
+
+print("    A      F F F F F");
+print("  A   A    F        ");
+print("A       A  F        ");
+print("A A A A A  F F F    ");
+print("A       A  F        ");
+print("A       A  F        ");
+print("A       A  F        ");
