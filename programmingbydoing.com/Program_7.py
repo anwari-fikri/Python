8eff1620708a34aaf0d6ca10b0bd2562c757fd09
anwari-fikri <50336496+anwari-fikri@users.noreply.github.com>
2019-05-07 22:00:19 +0800

    Add files via upload

--- programmingbydoing.com/Program_7.py
+++ programmingbydoing.com/Program_7.py
@@ -1,9 +1,9 @@
 # Your Initials
 
-print("    A      F F F F F");
-print("  A   A    F        ");
-print("A       A  F        ");
-print("A A A A A  F F F    ");
-print("A       A  F        ");
-print("A       A  F        ");
-print("A       A  F        ");
+print("    A      F F F F F")
+print("  A   A    F        ")
+print("A       A  F        ")
+print("A A A A A  F F F    ")
+print("A       A  F        ")
+print("A       A  F        ")
+print("A       A  F        ")
---
+++ programmingbydoing.com/Program_8.py
@@ -0,0 +1,6 @@
+# Numbers and Math
+
+print("I will now count my chickens:")
+
+print("Hens ", (25 + 30 / 6))
+print("Roosters ", (100 - 25 * 3 % 4))
