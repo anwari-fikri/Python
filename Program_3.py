51349d746fd0259ae98cd8b1fb2448d9e75d5ea6
anwari-fikri <50336496+anwari-fikri@users.noreply.github.com>
2019-05-07 17:48:07 +0800

    Add files via upload

---
+++ Program_3.py
@@ -0,0 +1,3 @@
+# An Important Message
+
+print("Hello, World!");
---
+++ Program_5.py
@@ -0,0 +1,13 @@
+# Comments and Slashes
+
+# Anwari Fikri 7/May/2019
+
+# A comment, this is so you can read your program later
+# Anything after the # is ignored by Python
+
+print("I could have a code like this."); # and the comment after is ignored.
+
+# You can also use a comment to "disable" or comment out a piece of code
+# print("This won't run.");
+
+print("This will run.");
